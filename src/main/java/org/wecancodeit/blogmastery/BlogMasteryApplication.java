c6d149949f631faf159b694f2aa160fd39a63782
Anthony Lam <lamah314@gmail.com>
2019-03-08 15:21:33 -0500

    initial commit

---
+++ src/main/java/org/wecancodeit/blogmastery/BlogMasteryApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.blogmastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogMasteryApplication.class, args);
+	}
+
+}
