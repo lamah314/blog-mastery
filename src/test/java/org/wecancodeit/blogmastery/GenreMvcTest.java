2d0e3c60104a3d8849190dd7218e6fcf4e8ea9f3
tulasi <tulasipinjala.com>
2019-03-10 12:30:25 -0400

    added all mvc file in eclipse

--- src/test/java/org/wecancodeit/blogmastery/GenreMvcTest.java
+++ src/test/java/org/wecancodeit/blogmastery/GenreMvcTest.java
@@ -3,9 +3,9 @@ package org.wecancodeit.blogmastery;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 import java.util.Optional;
 
@@ -18,8 +18,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.wecancodeit.blogmastery.controllers.AuthorController;
-import org.wecancodeit.blogmastery.models.Author;
+import org.wecancodeit.blogmastery.controllers.PostController;
+import org.wecancodeit.blogmastery.models.Genre;
 import org.wecancodeit.blogmastery.models.Post;
 import org.wecancodeit.blogmastery.repositories.AuthorRepository;
 import org.wecancodeit.blogmastery.repositories.GenreRepository;
@@ -28,8 +28,8 @@ import org.wecancodeit.blogmastery.repositories.TagRepository;
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(AuthorController.class)
-public class AuthorMvcTest<Portfolio> {
+@WebMvcTest(PostController.class)
+public class GenreMvcTest<Portfolio> {
 	 
 	    @Resource
 	    private MockMvc mvc;
@@ -47,26 +47,26 @@ public class AuthorMvcTest<Portfolio> {
 	    private TagRepository tagRepo;
 	 
 	    @Mock
-	    private Author author;
+	    private Genre genre;
 	    
 	    @Test
 	    public void shouldRouteToIndiviualPostView() throws Exception {
-	        Long authorId = 1L;
-	        when(authorRepo.findById(authorId)).thenReturn(Optional.of(author));
-	        mvc.perform(get("/author/1")).andExpect(view().name(is("/author/individualAuthor")));
+	        Long genreId = 1L;
+	        when(genreRepo.findById(genreId)).thenReturn(Optional.of(genre));
+	        mvc.perform(get("/genre/1")).andExpect(view().name(is("/genre/individualGenre")));
 	    }
 	     
 	    @Test
 	    public void shouldBeOkForIndividualPost() throws Exception {
-	    	Long authorId = 1L;
-	        when(authorRepo.findById(authorId)).thenReturn(Optional.of(author));
-	    	mvc.perform(get("/author/1")).andExpect(status().isOk());
+	    	Long genreId = 1L;
+	        when(genreRepo.findById(genreId)).thenReturn(Optional.of(genre));
+	    	mvc.perform(get("/genre/1")).andExpect(status().isOk());
     }
      
 	    @Test
 	    public void shouldPutSinglePostIntoModel() throws Exception {
-	    	Long authorId = 1L;
-	        when(authorRepo.findById(authorId)).thenReturn(Optional.of(author));
-	        mvc.perform(get("/author/1")).andExpect(model().attribute("author", is(author)));
+	    	Long genreId = 1L;
+	        when(genreRepo.findById(genreId)).thenReturn(Optional.of(genre));
+	        mvc.perform(get("/genre/1")).andExpect(model().attribute("genre", is(genre)));
     }
 }
--- src/test/java/org/wecancodeit/blogmastery/TagMvcTest.java
+++ src/test/java/org/wecancodeit/blogmastery/TagMvcTest.java
@@ -3,9 +3,9 @@ package org.wecancodeit.blogmastery;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 import java.util.Optional;
 
@@ -18,9 +18,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.wecancodeit.blogmastery.controllers.AuthorController;
-import org.wecancodeit.blogmastery.models.Author;
+import org.wecancodeit.blogmastery.controllers.PostController;
 import org.wecancodeit.blogmastery.models.Post;
+import org.wecancodeit.blogmastery.models.Tag;
 import org.wecancodeit.blogmastery.repositories.AuthorRepository;
 import org.wecancodeit.blogmastery.repositories.GenreRepository;
 import org.wecancodeit.blogmastery.repositories.PostRepository;
@@ -28,8 +28,8 @@ import org.wecancodeit.blogmastery.repositories.TagRepository;
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(AuthorController.class)
-public class AuthorMvcTest<Portfolio> {
+@WebMvcTest(PostController.class)
+public class TagMvcTest<Portfolio> {
 	 
 	    @Resource
 	    private MockMvc mvc;
@@ -47,26 +47,27 @@ public class AuthorMvcTest<Portfolio> {
 	    private TagRepository tagRepo;
 	 
 	    @Mock
-	    private Author author;
+	    private Tag tag;
 	    
 	    @Test
 	    public void shouldRouteToIndiviualPostView() throws Exception {
-	        Long authorId = 1L;
-	        when(authorRepo.findById(authorId)).thenReturn(Optional.of(author));
-	        mvc.perform(get("/author/1")).andExpect(view().name(is("/author/individualAuthor")));
+	        Long tagId = 1L;
+	        when(tagRepo.findById(tagId)).thenReturn(Optional.of(tag));
+	        mvc.perform(get("/tag/1")).andExpect(view().name(is("/tag/individualTag")));
 	    }
 	     
 	    @Test
 	    public void shouldBeOkForIndividualPost() throws Exception {
-	    	Long authorId = 1L;
-	        when(authorRepo.findById(authorId)).thenReturn(Optional.of(author));
-	    	mvc.perform(get("/author/1")).andExpect(status().isOk());
+	    	Long tagId = 1L;
+	        when(tagRepo.findById(tagId)).thenReturn(Optional.of(tag));
+	    	mvc.perform(get("/tag/1")).andExpect(status().isOk());
     }
      
 	    @Test
 	    public void shouldPutSinglePostIntoModel() throws Exception {
-	    	Long authorId = 1L;
-	        when(authorRepo.findById(authorId)).thenReturn(Optional.of(author));
-	        mvc.perform(get("/author/1")).andExpect(model().attribute("author", is(author)));
+	    	Long tagId = 1L;
+	        when(tagRepo.findById(tagId)).thenReturn(Optional.of(tag));
+	        mvc.perform(get("/tag/1")).andExpect(model().attribute("post", is(tag)));
     }
 }
+
